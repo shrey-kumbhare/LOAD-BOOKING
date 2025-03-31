c5d8d1526bd40bf7dc291b84196104b1645f1ce2
shrey-kumbhare <shrey@abilityex.com>
2025-03-31 16:38:45 +0530

    Initial Files

---
+++ src/main/java/com/example/load_booking_management/LoadBookingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.load_booking_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBookingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBookingManagementApplication.class, args);
+	}
+
+}
